0d5eff85bdb2a7645b608d433837783df1a241c8
prabhusadasivam <97303592+PRABHUNMK@users.noreply.github.com>
2022-09-25 21:34:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App(names) {
+  return (
+    <div>
+      <h1> i am prabu from app.js {names.name}</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+ReactDOM.render(
+  <div>
+    <App name="prabhu" />
+    <h1>hello </h1>
+  </div>,
+  document.getElementById("root")
+);
